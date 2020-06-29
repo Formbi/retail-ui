Formbi/retail-ui

commit e2195058c70424dc7e097cc8d174afbb05a9fd7d

feat: move all theme variables inside js, remove less support
also improved theme typings

packages/react-ui/internal/themes/FlatTheme.ts
@@ -1,0 +1,299 @@
+import { DefaultThemeInternal } from './DefaultTheme';
+
+// TODO Delete repeated values from flat theme
+
+export class FlatTheme extends (class {} as typeof DefaultThemeInternal) {
+  public static outlineColorFocus = '#fff';
+  public static borderColorFocus = '#1d85d0';
+  public static blinkColor = 'rgba(0, 136, 255, 0.2)';
+  public static btnHeightShift = '0';
+  public static btnWrapPadding = '0';
+  public static btnLinkBorderRadius = '2px';
+  public static btnArrowBorderRadius = '2px 2px 2px 16px';
+  public static btnSmallBorderRadius = '2px';
+  public static btnFocusShadowWidth = '1px';
+  public static btnCheckedBg = '#7e7e7e';
+  public static btnCheckedTextColor = '#fff';
+  public static btnCheckedShadow = 'none';
+  public static btnDisabledBg = '#f2f2f2';
+  public static btnDisabledShadow = 'none';
+  public static btnDisabledShadowColor = '#f2f2f2';
+  public static btnDefaultBgArrowStart = '#fff';
+  public static btnDefaultBgArrowEnd = '#fff';
+  public static btnDefaultTextColor = 'none';
+  public static btnDefaultHoverBg = 'rgba(0, 0, 0, 0.05)';
+  public static btnDefaultHoverBorderColor = 'rgba(0, 0, 0, 0.15)';
+  public static btnDefaultActiveBg = '#e5e5e5';
+  public static btnDefaultActiveBorderColor = '#cccccc';
+  public static btnDefaultBorder = '1px solid rgba(0, 0, 0, 0.15)';
+  public static btnDefaultShadow = 'none';
+  public static btnDefaultShadowArrow = '1px -1px 0 0 rgba(0, 0, 0, 0.15)';
+  public static btnDefaultBgStart = 'none';
+  public static btnDefaultBgEnd = 'none';
+  public static btnDefaultHoverBgStart = '#f2f2f2';
+  public static btnDefaultHoverBgEnd = '#f2f2f2';
+  public static btnDefaultHoverShadow = 'none';
+  public static btnDefaultActiveShadow = 'none';
+  public static btnPrimaryBg = '#1d85d0';
+  public static btnPrimaryTextColor = '#fff';
+  public static btnPrimaryHoverBg = '#1b7dc3';
+  public static btnPrimaryActiveBg = '#1974b6';
+  public static btnPrimaryDisabledBg = '#1d85d0';
+  public static btnPrimaryBorder = '1px solid transparent';
+  public static btnPrimaryBgStart = '#1d85d0';
+  public static btnPrimaryBgEnd = '#1d85d0';
+  public static btnPrimaryBgArrowStart = '#1d85d0';
+  public static btnPrimaryBgArrowEnd = '#1d85d0';
+  public static btnPrimaryHoverBgStart = '#1b7dc3';
+  public static btnPrimaryHoverBgEnd = '#1b7dc3';
+  public static btnPrimaryShadow = 'none';
+  public static btnPrimaryHoverShadow = 'none';
+  public static btnPrimaryActiveShadow = 'none';
+  public static btnSuccessBg = '#419d14';
+  public static btnSuccessTextColor = '#fff';
+  public static btnSuccessHoverBg = '#3c9312';
+  public static btnSuccessActiveBg = '#398911';
+  public static btnSuccessDisabledBg = '#419d14';
+  public static btnSuccessBorder = '1px solid transparent';
+  public static btnSuccessBgStart = '#419d14';
+  public static btnSuccessBgEnd = '#419d14';
+  public static btnSuccessBgArrowStart = '#419d14';
+  public static btnSuccessBgArrowEnd = '#419d14';
+  public static btnSuccessHoverBgStart = '#3c9312';
+  public static btnSuccessHoverBgEnd = '#3c9312';
+  public static btnSuccessShadow = 'none';
+  public static btnSuccessHoverShadow = 'none';
+  public static btnSuccessActiveShadow = 'none';
+  public static btnDangerBg = '#d9472b';
+  public static btnDangerTextColor = '#fff';
+  public static btnDangerHoverBg = '#cc4228';
+  public static btnDangerActiveBg = '#be3e25';
+  public static btnDangerDisabledBg = '#d9472b';
+  public static btnDangerBorder = '1px solid transparent';
+  public static btnDangerBgStart = '#d9472b';
+  public static btnDangerBgEnd = '#d9472b';
+  public static btnDangerBgArrowStart = '#d9472b';
+  public static btnDangerBgArrowEnd = '#d9472b';
+  public static btnDangerHoverBgStart = '#cc4228';
+  public static btnDangerHoverBgEnd = '#cc4228';
+  public static btnDangerShadow = 'none';
+  public static btnDangerHoverShadow = 'none';
+  public static btnDangerActiveShadow = 'none';
+  public static btnPayBg = '#ffca43';
+  public static btnPayTextColor = 'none';
+  public static btnPayHoverBg = '#f0be3f';
+  public static btnPayActiveBg = '#e0b13a';
+  public static btnPayDisabledBg = '#ffca43';
+  public static btnPayBorder = '1px solid transparent';
+  public static btnPayBgStart = '#ffca43';
+  public static btnPayBgEnd = '#ffca43';
+  public static btnPayBgArrowStart = '#ffca43';
+  public static btnPayBgArrowEnd = '#ffca43';
+  public static btnPayHoverBgStart = '#f0be3f';
+  public static btnPayHoverBgEnd = '#f0be3f';
+  public static btnPayShadow = 'none';
+  public static btnPayHoverShadow = 'none';
+  public static btnPayActiveShadow = 'none';
+  public static btnSmallArrowLength = '17px';
+  public static btnSmallArrowTop = '7.5px';
+  public static btnSmallArrowRight = '-8px';
+  public static btnSmallArrowLeft = '-8.2px';
+  public static btnSmallArrowLeftLoadingDelay = '0.23s';
+  public static btnSmallArrowBorderRadius = '2px 2px 4px 16px';
+  public static btnSmallArrowBg = 'linear-gradient(-53deg, transparent 48%, #ccc 0, #ccc 72%, transparent 0)';
+  public static btnMediumArrowLeft = '-10px';
+  public static btnMediumArrowRight = '-10px';
+  public static btnMediumArrowLeftLoadingLeft = '-207px';
+  public static btnMediumArrowTransform = 'rotate(53deg) skewX(25deg) skewY(8deg)';
+  public static btnMediumArrowBg = 'linear-gradient(-56deg, transparent 48%, #ccc 0, #ccc 70%, transparent 0)';
+  public static btnMediumArrowLeftLoadingDelay = '0.45s';
+  public static btnDeprecatedSizeMediumFontSize = '16px';
+  public static btnDeprecatedSizeMediumPaddingShift = '1';
+  public static btnDeprecatedSizeMediumPaddingShiftIe = '2';
+  public static btnLargeArrowLeft = '-10.8px';
+  public static btnLargeArrowTransform = 'rotate(53deg) skewX(26deg) skewY(10deg)';
+  public static btnLargeArrowBg = 'linear-gradient(-56deg, transparent 48.2%, #ccc 0, #ccc 63.4%, transparent 0)';
+  public static btnLargeArrowLeftLoadingDelay = '0s';
+  public static chbShadow = '0 0 0 1px rgba(0, 0, 0, 0.15)';
+  public static chbBg = 'none';
+  public static chbIndeterminateBg = '#fff';
+  public static chbShadowDisabled = '0 0 0 1px #f2f2f2';
+  public static chbShadowActive = '0 0 0 1px #c3c3c3';
+  public static chbShadowHover = '0 0 0 1px #c3c3c3';
+  public static chbShadowWidth = '2px';
+  public static chbCheckedColor = '#fff';
+  public static chbCheckedShadow = '0 0 0 1px #1768a3';
+  public static chbCheckedHoverShadow = '0 0 0 1px #175590';
+  public static chbBorderRadius = '1px';
+  public static inputBg = 'white';
+  public static inputIconColor = '#a9a9a9';
+  public static inputColor = 'inherit';
+  public static inputShadow = 'none';
+  public static inputBorderWidth = '1px';
+  public static inputBorderTopColor = 'rgba(0, 0, 0, 0.15)';
+  public static inputDisabledBg = 'rgba(0, 0, 0, 0.05)';
+  public static toggleBg = '#fff';
+  public static toggleBgDisabled = 'none';
+  public static toggleBgHover = '#f3f3f2';
+  public static toggleBgChecked = '#3072c4';
+  public static toggleBgWarning = '#f69c00';
+  public static toggleBgError = '#d70c17';
+  public static toggleBgActive = '#e5e5e5';
+  public static toggleBorderColor = '#d0d0d0';
+  public static textareaBg = 'white';
+  public static textareaColor = 'inherit';
+  public static textareaShadow = 'none';
+  public static textareaBorderTopColor = 'rgba(0, 0, 0, 0.15)';
+  public static textareaDisabledBg = 'rgba(0, 0, 0, 0.05)';
+  public static radioSize = '18px';
+  public static radioVerticalAlign = 'top';
+  public static radioBgImage = 'none';
+  public static radioBoxShadow = 'none';
+  public static radioBorder = '1px solid rgba(0, 0, 0, 0.15)';
+  public static radioHoverShadow = 'none';
+  public static radioActiveShadow = 'none';
+  public static radioCheckedBulletColor = '#fff';
+  public static radioDisabledShadow = 'none';
+  public static radioLabelDisplay = 'inline-block';
+  public static btnFontSizeMedium = '16px';
+  public static get btnCheckedShadowArrow() {
+    return `0 0 0 1px ${this.btnCheckedBg}`;
+  }
+  public static get btnCheckedShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnCheckedBg}`;
+  }
+  public static get btnFocusBorder() {
+    return `1px solid ${this.borderColorFocus}`;
+  }
+  public static get btnCheckedHoverBorderColor() {
+    return this.btnCheckedBg;
+  }
+  public static get btnDefaultShadowArrowLeft() {
+    return this.btnDefaultShadowArrow;
+  }
+  public static get btnDefaultCheckedShadowArrow() {
+    return `1px -1px 0 0 rgba(0, 0, 0, 0.15), 0 0 0 1px ${this.btnCheckedBg}`;
+  }
+  public static get btnDefaultHoverShadowArrow() {
+    return `1px -1px 0 0 ${this.btnDefaultHoverBorderColor}, 1px -1px 0 0 ${this.btnDefaultHoverBg}`;
+  }
+  public static get btnDefaultHoverShadowArrowLeft() {
+    return `1px -1px 0 0 ${this.btnDefaultHoverBorderColor}, 1px -1px 0 0 ${this.btnDefaultHoverBg}`;
+  }
+  public static get btnDefaultActiveShadowArrow() {
+    return `1px -1px 0 0 ${this.btnDefaultActiveBorderColor}, 1px -1px 0 0 ${this.btnDefaultActiveBg}`;
+  }
+  public static get btnDefaultActiveShadowArrowLeft() {
+    return `1px -1px 0 0 ${this.btnDefaultActiveBorderColor}, 1px -1px 0 0 ${this.btnDefaultActiveBg}`;
+  }
+  public static get btnPrimaryShadowArrow() {
+    return `0 0 0 1px ${this.btnPrimaryDisabledBg}`;
+  }
+  public static get btnPrimaryShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnPrimaryDisabledBg}`;
+  }
+  public static get btnPrimaryHoverShadowArrow() {
+    return `0 0 0 1px ${this.btnPrimaryHoverBg}`;
+  }
+  public static get btnPrimaryHoverShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnPrimaryHoverBg}`;
+  }
+  public static get btnPrimaryActiveShadowArrow() {
+    return `0 0 0 1px ${this.btnPrimaryActiveBg}`;
+  }
+  public static get btnPrimaryActiveShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnPrimaryActiveBg}`;
+  }
+  public static get btnSuccessShadowArrow() {
+    return `0 0 0 1px ${this.btnSuccessDisabledBg}`;
+  }
+  public static get btnSuccessShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnSuccessDisabledBg}`;
+  }
+  public static get btnSuccessHoverShadowArrow() {
+    return `0 0 0 1px ${this.btnSuccessHoverBg}`;
+  }
+  public static get btnSuccessHoverShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnSuccessHoverBg}`;
+  }
+  public static get btnSuccessActiveShadowArrow() {
+    return `0 0 0 1px ${this.btnSuccessActiveBg}`;
+  }
+  public static get btnSuccessActiveShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnSuccessActiveBg}`;
+  }
+  public static get btnDangerShadowArrow() {
+    return `0 0 0 1px ${this.btnDangerDisabledBg}`;
+  }
+  public static get btnDangerShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnDangerDisabledBg}`;
+  }
+  public static get btnDangerHoverShadowArrow() {
+    return `0 0 0 1px ${this.btnDangerHoverBg}`;
+  }
+  public static get btnDangerHoverShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnDangerHoverBg}`;
+  }
+  public static get btnDangerActiveShadowArrow() {
+    return `0 0 0 1px ${this.btnDangerActiveBg}`;
+  }
+  public static get btnDangerActiveShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnDangerActiveBg}`;
+  }
+  public static get btnPayShadowArrow() {
+    return `0 0 0 1px ${this.btnPayDisabledBg}`;
+  }
+  public static get btnPayShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnPayDisabledBg}`;
+  }
+  public static get btnPayHoverShadowArrow() {
+    return `0 0 0 1px ${this.btnPayHoverBg}`;
+  }
+  public static get btnPayHoverShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnPayHoverBg}`;
+  }
+  public static get btnPayActiveShadowArrow() {
+    return `0 0 0 1px ${this.btnPayActiveBg}`;
+  }
+  public static get btnPayActiveShadowArrowLeft() {
+    return `0 0 0 1px ${this.btnPayActiveBg}`;
+  }
+  public static get inputDisabledBorderColor() {
+    return this.inputDisabledBg;
+  }
+  public static get chbBorder() {
+    return this.btnDefaultBorder;
+  }
+  public static get chbBorderColorFocus() {
+    return this.borderColorFocus;
+  }
+  public static get chbHoverBg() {
+    return this.btnDefaultHoverBg;
+  }
+  public static get chbActiveBg() {
+    return this.btnDefaultActiveBg;
+  }
+  public static get chbCheckedBg() {
+    return this.borderColorFocus;
+  }
+  public static get chbBoxIndeterminateBg() {
+    return this.chbCheckedBg;
+  }
+  public static get radioHoverBg() {
+    return this.chbHoverBg;
+  }
+  public static get radioActiveBg() {
+    return this.chbActiveBg;
+  }
+  public static get radioFocusShadow() {
+    return `inset 0 0 0 1px ${this.outlineColorFocus}`;
+  }
+  public static get radioCheckedBgColor() {
+    return this.chbCheckedBg;
+  }
+  public static get textareaDisabledBorderColor() {
+    return this.textareaDisabledBg;
+  }
+}
+
+export const FlatThemeInternal: typeof FlatTheme = Object.setPrototypeOf(FlatTheme, DefaultThemeInternal);

packages/react-ui/lib/theming/ThemeHelpers.ts
@@ -1,21 +1,0 @@
-const IS_COMPUTED_MARKER = '__COMPUTED__';
-
-export function defineInternalTheme<VM, CD extends PropertyDescriptorMap>(
-  staticVariables: VM,
-  computedDescriptors: CD & ThisType<VM & CD>,
-) {
-  const result: { [key: string]: string } = {};
-
-  Object.assign(result, staticVariables);
-
-  Object.keys(computedDescriptors).forEach(key => {
-    const resultElement = result[key];
-
-    if (!resultElement || resultElement === IS_COMPUTED_MARKER) {
-      computedDescriptors[key].enumerable = true;
-      Object.defineProperty(result, key, computedDescriptors[key]);
-    }
-  });
-
-  return Object.freeze(result) as { readonly [key in keyof (VM & CD)]: string };
-}

packages/react-ui/lib/theming/themes/DefaultTheme.ts
@@ -1,348 +1,3 @@
-import DEFAULT_VARIABLES from '../../../components/variables.module.less';
-import { defineInternalTheme } from '../ThemeHelpers';
-import * as ColorFunctions from '../../styles/ColorFunctions';
+import { ThemeFactory } from '../ThemeFactory';
 
-export const DEFAULT_THEME = defineInternalTheme(DEFAULT_VARIABLES, {
-  bgActive: {
-    get() {
-      return this.blueLight;
-    },
-  },
-  borderColorFocus: {
-    get() {
-      return this.blueLight;
-    },
-  },
-  borderColorError: {
-    get() {
-      return this.errorMain;
-    },
-  },
-  borderColorWarning: {
-    get() {
-      return this.warningMain;
-    },
-  },
-  btnFontSizeSmall: {
-    get() {
-      return this.fontSizeSmall;
-    },
-  },
-  btnFontSizeMedium: {
-    get() {
-      return this.fontSizeMedium;
-    },
-  },
-  btnFontSizeLarge: {
-    get() {
-      return this.fontSizeLarge;
-    },
-  },
-  btnPaddingYSmall: {
-    get() {
-      return this.controlPaddingYSmall;
-    },
-  },
-  btnPaddingYMedium: {
-    get() {
-      return this.controlPaddingYMedium;
-    },
-  },
-  btnPaddingYLarge: {
-    get() {
-      return this.controlPaddingYLarge;
-    },
-  },
-  btnDisabledBg: {
-    get() {
-      return this.bgDisabled;
-    },
-  },
-  controlHeightSmall: {
-    get() {
-      const borderWidth = parseInt(this.controlBorderWidth, 10) || 0;
-      const paddingYSmall = parseInt(this.controlPaddingYSmall, 10) || 0;
-      const lineHeightSmall = parseInt(this.controlLineHeightSmall, 10) || 0;
-      return `${2 * borderWidth + 2 * paddingYSmall + lineHeightSmall}px`;
-    },
-  },
-  controlHeightMedium: {
-    get() {
-      const borderWidth = parseInt(this.controlBorderWidth, 10) || 0;
-      const paddingYMedium = parseInt(this.controlPaddingYMedium, 10) || 0;
-      const lineHeightMedium = parseInt(this.controlLineHeightMedium, 10) || 0;
-      return `${2 * borderWidth + 2 * paddingYMedium + lineHeightMedium}px`;
-    },
-  },
-  controlHeightLarge: {
-    get() {
-      const borderWidth = parseInt(this.controlBorderWidth, 10) || 0;
-      const paddingYLarge = parseInt(this.controlPaddingYLarge, 10) || 0;
-      const lineHeightLarge = parseInt(this.controlLineHeightLarge, 10) || 0;
-      return `${2 * borderWidth + 2 * paddingYLarge + lineHeightLarge}px`;
-    },
-  },
-  linkHoverColor: {
-    get() {
-      return this.linkColor;
-    },
-  },
-  linkDisabledColor: {
-    get() {
-      return this.textColorDisabled;
-    },
-  },
-  dropdownMenuHoverBg: {
-    get() {
-      return this.bgActive;
-    },
-  },
-  btnDisabledShadowColor: {
-    get() {
-      return this.borderColorGrayLight;
-    },
-  },
-  btnDisabledShadow: {
-    get() {
-      return `0 0 0 1px ${this.btnDisabledShadowColor}`;
-    },
-  },
-  btnDisabledShadowArrow: {
-    get() {
-      return `1px -1px 0 0 ${this.btnDisabledShadowColor}`;
-    },
-  },
-  tabColorFocus: {
-    get() {
-      return this.borderColorFocus;
-    },
-  },
-  tabColorError: {
-    get() {
-      return this.btnDangerBg;
-    },
-  },
-  tabColorWarning: {
-    get() {
-      return this.btnPayBg;
-    },
-  },
-  tabColorSuccess: {
-    get() {
-      return this.btnSuccessBg;
-    },
-  },
-  tabColorPrimary: {
-    get() {
-      return this.btnPrimaryBg;
-    },
-  },
-  tabColorHover: {
-    get() {
-      return this.borderColorFocusLight;
-    },
-  },
-  tabColorHoverError: {
-    get() {
-      return ColorFunctions.lighten(this.tabColorError, '25%');
-    },
-  },
-  tabColorHoverWarning: {
-    get() {
-      return ColorFunctions.lighten(this.tabColorWarning, '25%');
-    },
-  },
-  tabColorHoverSuccess: {
-    get() {
-      return ColorFunctions.lighten(this.tabColorSuccess, '25%');
-    },
-  },
-  tabColorHoverPrimary: {
-    get() {
-      return ColorFunctions.lighten(this.tabColorPrimary, '25%');
-    },
-  },
-  calendarCellHoverBgColor: {
-    get() {
-      return this.bgActive;
-    },
-  },
-  dateSelectMenuBg: {
-    get() {
-      return this.bgDefault;
-    },
-  },
-  dateSelectMenuItemBgActive: {
-    get() {
-      return this.bgActive;
-    },
-  },
-  dateSelectMenuItemBgDisabled: {
-    get() {
-      return this.bgDefault;
-    },
-  },
-  dateSelectMenuItemFontActive: {
-    get() {
-      return this.textColorInvert;
-    },
-  },
-  dateSelectMenuItemFontSelected: {
-    get() {
-      return this.textColorDefault;
-    },
-  },
-  dateSelectMenuItemFontDisabled: {
-    get() {
-      return this.textColorDisabled;
-    },
-  },
-  pagingForwardLinkColor: {
-    get() {
-      return this.linkColor;
-    },
-  },
-  pagingForwardLinkDisabledColor: {
-    get() {
-      return this.linkDisabledColor;
-    },
-  },
-  toggleBgActive: {
-    get() {
-      return this.toggleBgChecked;
-    },
-  },
-  toggleBgError: {
-    get() {
-      return this.errorMain;
-    },
-  },
-  toggleBgWarning: {
-    get() {
-      return this.warningMain;
-    },
-  },
-  toggleFocusShadowColor: {
-    get() {
-      return this.borderColorFocus;
-    },
-  },
-  inputFontSizeSmall: {
-    get() {
-      return this.fontSizeSmall;
-    },
-  },
-  inputFontSizeMedium: {
-    get() {
-      return this.fontSizeMedium;
-    },
-  },
-  inputFontSizeLarge: {
-    get() {
-      return this.fontSizeLarge;
-    },
-  },
-  inputFocusShadow: {
-    get() {
-      return `0 0 0 1px ${this.borderColorFocus}`;
-    },
-  },
-  inputDisabledBg: {
-    get() {
-      return this.bgDisabled;
-    },
-  },
-  inputDisabledBorderColor: {
-    get() {
-      return this.borderColorGrayLight;
-    },
-  },
-  inputFocusOutline: {
-    get() {
-      return this.borderColorFocus;
-    },
-  },
-  inputBorderWidth: {
-    get() {
-      return this.controlBorderWidth;
-    },
-  },
-  inputBorderTopColor: {
-    get() {
-      return this.borderColorGrayDark;
-    },
-  },
-  chbBorderColorFocus: {
-    get() {
-      return this.borderColorFocus;
-    },
-  },
-  chbBg: {
-    get() {
-      return `linear-gradient(${this.chbBgStart}, ${this.chbBgEnd})`;
-    },
-  },
-  chbHoverBg: {
-    get() {
-      return `linear-gradient(-180deg, ${this.btnDefaultHoverBgStart} 0, ${this.btnDefaultHoverBgEnd} 100%)`;
-    },
-  },
-  chbActiveBg: {
-    get() {
-      return this.btnDefaultActiveBg;
-    },
-  },
-  chbShadowActive: {
-    get() {
-      return this.btnDefaultActiveShadow;
-    },
-  },
-  textareaLineHeight: {
-    get() {
-      const fontSize = this.fontSizeMedium;
-      switch (fontSize) {
-        case '14px':
-          return '20px';
-        case '16px':
-          return '25px';
-        default:
-          return `${parseInt(fontSize, 10) * 1.5}px`;
-      }
-    },
-  },
-  textareaBorderTopColor: {
-    get() {
-      return this.borderColorGrayDark;
-    },
-  },
-  textareaDisabledBg: {
-    get() {
-      return this.bgDisabled;
-    },
-  },
-  textareaDisabledBorderColor: {
-    get() {
-      return this.borderColorGrayLight;
-    },
-  },
-  spinnerBgColor: {
-    get() {
-      return this.grayXLight;
-    },
-  },
-  spinnerDimmedColor: {
-    get() {
-      return this.gray;
-    },
-  },
-  spinnerCaptionColor: {
-    get() {
-      return this.gray;
-    },
-  },
-  popupBackground: {
-    get() {
-      return this.bgDefault;
-    },
-  },
-});
+export const DEFAULT_THEME = ThemeFactory.create({});

packages/react-ui/lib/theming/themes/FlatTheme.ts
@@ -1,241 +1,4 @@
-import FLAT_VARIABLES from '../../../components/variables.flat.module.less';
-import { defineInternalTheme } from '../ThemeHelpers';
+import { FlatThemeInternal } from '../../../internal/themes/FlatTheme';
+import { ThemeFactory } from '../ThemeFactory';
 
-export const FLAT_THEME = defineInternalTheme(FLAT_VARIABLES, {
-  btnFontSizeMedium: {
-    get() {
-      return '16px';
-    },
-  },
-  btnCheckedShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnCheckedBg}`;
-    },
-  },
-  btnCheckedShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnCheckedBg}`;
-    },
-  },
-  btnFocusBorder: {
-    get() {
-      return `1px solid ${this.borderColorFocus}`;
-    },
-  },
-  btnCheckedHoverBorderColor: {
-    get() {
-      return this.btnCheckedBg;
-    },
-  },
-  btnDefaultShadowArrowLeft: {
-    get() {
-      return this.btnDefaultShadowArrow;
-    },
-  },
-  btnDefaultCheckedShadowArrow: {
-    get() {
-      return `1px -1px 0 0 rgba(0, 0, 0, 0.15), 0 0 0 1px ${this.btnCheckedBg}`;
-    },
-  },
-
-  btnDefaultHoverShadowArrow: {
-    get() {
-      return `1px -1px 0 0 ${this.btnDefaultHoverBorderColor}, 1px -1px 0 0 ${this.btnDefaultHoverBg};`;
-    },
-  },
-  btnDefaultHoverShadowArrowLeft: {
-    get() {
-      return `1px -1px 0 0 ${this.btnDefaultHoverBorderColor}, 1px -1px 0 0 ${this.btnDefaultHoverBg};`;
-    },
-  },
-  btnDefaultActiveShadowArrow: {
-    get() {
-      return `1px -1px 0 0 ${this.btnDefaultActiveBorderColor}, 1px -1px 0 0 ${this.btnDefaultActiveBg};`;
-    },
-  },
-  btnDefaultActiveShadowArrowLeft: {
-    get() {
-      return `1px -1px 0 0 ${this.btnDefaultActiveBorderColor}, 1px -1px 0 0 ${this.btnDefaultActiveBg};`;
-    },
-  },
-  btnPrimaryShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnPrimaryDisabledBg};`;
-    },
-  },
-  btnPrimaryShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnPrimaryDisabledBg};`;
-    },
-  },
-  btnPrimaryHoverShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnPrimaryHoverBg};`;
-    },
-  },
-  btnPrimaryHoverShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnPrimaryHoverBg};`;
-    },
-  },
-  btnPrimaryActiveShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnPrimaryActiveBg};`;
-    },
-  },
-  btnPrimaryActiveShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnPrimaryActiveBg};`;
-    },
-  },
-  btnSuccessShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnSuccessDisabledBg};`;
-    },
-  },
-  btnSuccessShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnSuccessDisabledBg};`;
-    },
-  },
-  btnSuccessHoverShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnSuccessHoverBg};`;
-    },
-  },
-  btnSuccessHoverShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnSuccessHoverBg};`;
-    },
-  },
-  btnSuccessActiveShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnSuccessActiveBg};`;
-    },
-  },
-  btnSuccessActiveShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnSuccessActiveBg};`;
-    },
-  },
-  btnDangerShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnDangerDisabledBg};`;
-    },
-  },
-  btnDangerShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnDangerDisabledBg};`;
-    },
-  },
-  btnDangerHoverShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnDangerHoverBg};`;
-    },
-  },
-  btnDangerHoverShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnDangerHoverBg};`;
-    },
-  },
-  btnDangerActiveShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnDangerActiveBg};`;
-    },
-  },
-  btnDangerActiveShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnDangerActiveBg};`;
-    },
-  },
-  btnPayShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnPayDisabledBg};`;
-    },
-  },
-  btnPayShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnPayDisabledBg};`;
-    },
-  },
-  btnPayHoverShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnPayHoverBg};`;
-    },
-  },
-  btnPayHoverShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnPayHoverBg};`;
-    },
-  },
-  btnPayActiveShadowArrow: {
-    get() {
-      return `0 0 0 1px ${this.btnPayActiveBg};`;
-    },
-  },
-  btnPayActiveShadowArrowLeft: {
-    get() {
-      return `0 0 0 1px ${this.btnPayActiveBg};`;
-    },
-  },
-  inputDisabledBorderColor: {
-    get() {
-      return this.inputDisabledBg;
-    },
-  },
-  chbBorder: {
-    get() {
-      return this.btnDefaultBorder;
-    },
-  },
-  chbBorderColorFocus: {
-    get() {
-      return this.borderColorFocus;
-    },
-  },
-  chbHoverBg: {
-    get() {
-      return this.btnDefaultHoverBg;
-    },
-  },
-  chbActiveBg: {
-    get() {
-      return this.btnDefaultActiveBg;
-    },
-  },
-  chbCheckedBg: {
-    get() {
-      return this.borderColorFocus;
-    },
-  },
-  chbBoxIndeterminateBg: {
-    get() {
-      return this.chbCheckedBg;
-    },
-  },
-  radioHoverBg: {
-    get() {
-      return this.chbHoverBg;
-    },
-  },
-  radioActiveBg: {
-    get() {
-      return this.chbActiveBg;
-    },
-  },
-  radioFocusShadow: {
-    get() {
-      return `inset 0 0 0 1px ${this.outlineColorFocus};`;
-    },
-  },
-  radioCheckedBgColor: {
-    get() {
-      return this.chbCheckedBg;
-    },
-  },
-  textareaDisabledBorderColor: {
-    get() {
-      return this.textareaDisabledBg;
-    },
-  },
-});
+export const FLAT_THEME = ThemeFactory.create(FlatThemeInternal);

packages/react-ui/.babelrc.js
@@ -1,8 +1,5 @@
 module.exports = {
   env: {
-    production: {
-      plugins: ['./scripts/babel/imports-less-to-css.js'],
-    },
     development: {
       presets: [['@babel/preset-env', { loose: true, modules: 'commonjs', targets: { ie: '11' } }]],
       plugins: [['@babel/plugin-transform-runtime', { version: '7.8.3' }]],

packages/react-ui/.storybook/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = async ({ config, mode }) => {
       },
     },
     {
-      test: /\.(css|less)$/,
+      test: /\.css$/,
       loaders: [
         'style-loader',
         {
@@ -45,7 +45,6 @@ module.exports = async ({ config, mode }) => {
             },
           },
         },
-        'less-loader',
       ],
     },
     { test: /\.(woff|woff2|eot)$/, loader: 'file-loader' },

packages/react-ui/.styleguide/config/base.config.js
@@ -176,7 +176,7 @@ const webpackConfig = {
         },
       },
       {
-        test: /\.(css|less)$/,
+        test: /\.css$/,
         loaders: [
           'style-loader',
           {
@@ -194,7 +194,6 @@ const webpackConfig = {
               },
             },
           },
-          'less-loader',
         ],
       },
       {

commit 8504513e0f9fe4179e5a0b4de1f793075e44f440

fix(ThemeFactory): don't ignore computed properties in factory methods
was broken in e2195058

packages/react-ui/internal/themes/DefaultTheme.ts
@@ -1,4 +1,5 @@
 import * as ColorFunctions from '../../lib/styles/ColorFunctions';
+import { exposeGetters } from '../../lib/theming/ThemeHelpers';
 
 export class DefaultTheme {
   //#region Common variables
@@ -1282,4 +1283,4 @@ export class DefaultTheme {
   //#endregion
 }
 
-export const DefaultThemeInternal = DefaultTheme;
+export const DefaultThemeInternal = exposeGetters(DefaultTheme);

packages/react-ui/internal/themes/FlatTheme.ts
@@ -1,4 +1,5 @@
 import * as ColorFunctions from '../../lib/styles/ColorFunctions';
+import { exposeGetters } from '../../lib/theming/ThemeHelpers';
 
 import { DefaultThemeInternal } from './DefaultTheme';
 
@@ -339,4 +340,7 @@ export class FlatTheme extends (class {} as typeof DefaultThemeInternal) {
   //#endregion
 }
 
-export const FlatThemeInternal = Object.setPrototypeOf(FlatTheme, DefaultThemeInternal) as typeof FlatTheme;
+export const FlatThemeInternal = Object.setPrototypeOf(
+  exposeGetters(FlatTheme),
+  DefaultThemeInternal,
+) as typeof FlatTheme;

packages/react-ui/lib/theming/ThemeHelpers.ts
@@ -1,0 +1,11 @@
+export const exposeGetters = <T extends object>(theme: T): T => {
+  const descriptors = Object.getOwnPropertyDescriptors(theme);
+  Object.keys(descriptors).forEach(key => {
+    const descriptor = descriptors[key];
+    if (typeof descriptor.get === 'function' && descriptor.configurable) {
+      descriptor.enumerable = true;
+      Object.defineProperty(theme, key, descriptor);
+    }
+  });
+  return theme;
+};